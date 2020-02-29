513378f0735384aa53c70374dbcdbc702d00c01e
DaRkFoxDeveloper <darkfoxdeveloper@gmail.com>
2020-02-29 14:04:03 +0100

    Base

---
+++ SmartConquerLoader/SmartConquerLoader/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartConquerLoader
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            this.Icon = new Icon(@"Assets\co.ico");
+        }
+
+    }
+}
