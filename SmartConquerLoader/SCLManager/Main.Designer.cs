e877d9db74c32c02e5df774b50e4b04483195875
DaRkFoxDeveloper <darkfoxdeveloper@gmail.com>
2020-03-01 18:20:58 +0100

    Starting with SCLManager

--- SmartConquerLoader/SCLManager/Main.Designer.cs
+++ SmartConquerLoader/SCLManager/Main.Designer.cs
@@ -28,10 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(563, 60);
+            this.Font = new System.Drawing.Font("Caladea", 9.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "SmartConquerLoader";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- SmartConquerLoader/SCLManager/Main.cs
+++ SmartConquerLoader/SCLManager/Main.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using System.Windows.Forms;
 
 namespace SCLManager
@@ -15,7 +8,7 @@ namespace SCLManager
         public Main()
         {
             InitializeComponent();
+            this.Icon = new Icon(@"Assets\co.ico");
         }
-
     }
 }
